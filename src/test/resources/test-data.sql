d9d6c16ed171100386049196c05842fe29e7523c
hagyutae <gted221@gmail.com>
2025-11-06 15:16:07 +0900

    Post viewCount 필드 추가로 인한 테스트 코드 Fail 문제 해결

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -1,4 +1,4 @@
-INSERT INTO post (author, title, content, password)
+INSERT INTO post (author, title, content, password, view_count)
 VALUES
-    ('sql-tester', 'SQL Test', 'Content from SQL', '123456'),
-    ('sql-tester2', 'SQL Tes2t', 'Content from SQL', '123456');
+    ('sql-tester', 'SQL Test', 'Content from SQL', '123456', 0),
+    ('sql-tester2', 'SQL Tes2t', 'Content from SQL', '123456', 0);
